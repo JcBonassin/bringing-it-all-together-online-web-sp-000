f3a3eddc15327aa3bc74cfe46c3ecb749e43dcc8
JC <bonassin@gmail.com>
2021-02-23 17:28:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -65,4 +65,13 @@ def self.create_table
     dog
   end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
+
+  
+
 end
