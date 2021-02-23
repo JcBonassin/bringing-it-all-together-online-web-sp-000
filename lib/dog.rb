451aaca0280d66e59c3c124bac96451069f9a71c
JC <bonassin@gmail.com>
2021-02-23 17:29:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,6 +72,18 @@ def self.create_table
     self.new(id: id, name: name, breed: breed)
   end
 
-  
+  def self.find_by_id(id)
+      sql = <<-SQL
+        SELECT *
+        FROM dogs
+        WHERE id = ?
+        LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql,id).map do |row|
+        self.new_from_db(row)
+      end.first
+    end
+
 
 end
